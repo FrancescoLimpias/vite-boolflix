9030f8dc823edd8012e72e014aa4800586cc2791
Francesco Limpias <francescolimpias@gmail.com>
2022-12-19 16:38:50 +0100

    Added flag translation support

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,11 +1,25 @@
 <script>
 import { store } from '../store.js'
+import { localeAlpha2 } from '../flags.js'
 
 export default {
     data() {
         return {
             store,
+            localeAlpha2,
         }
+    },
+
+    methods: {
+        getFlagByLocale(locale) {
+
+            if (this.localeAlpha2[locale]) {
+                return `https://flagcdn.com/w20/${this.localeAlpha2[locale]}.png`;
+            } else {
+                return false;
+            }
+
+        },
     },
 }
 </script>
@@ -15,7 +29,11 @@ export default {
         <li v-for="res in store.search.results">
             Title: {{ res.title }}
             <br>Original Title: {{ res.original_title }}
-            <br>Original Lang: {{ res.original_language }}
+            <br>
+            <img v-if="getFlagByLocale(res.original_language)" :src="getFlagByLocale(res.original_language)" alt="">
+            <span v-else>
+                Original Lang: {{ res.original_language }}
+            </span>
             <br>Vote: {{ res.vote_average }}
         </li>
     </ul>
